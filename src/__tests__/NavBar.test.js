550d544410e0889425d006b4801f271f31a7e230
Christine <christinekhekama99@gmail.com>
2023-07-19 14:18:47 +0300

    navbar

--- src/__tests__/NavBar.test.js
+++ src/__tests__/NavBar.test.js
@@ -1,32 +1,13 @@
-import "@testing-library/jest-dom";
-import { render, screen } from "@testing-library/react";
-import NavBar from "../components/NavBar";
+function NavBar() {
+  const links = ["home", "about", "projects"];
+  const navLinks=links.map((link)=>{
+    return <a key={link} href={"#" + link}> {link}</a>
+  })
 
-test("each <a> element has a unique key prop", () => {
-  const errorSpy = jest.spyOn(global.console, "error");
+  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  return <nav>{/* display an <a> tag for each link here */}
+  {navLinks}
+  </nav>;
+}
 
-  render(<NavBar />);
-
-  expect(errorSpy).not.toHaveBeenCalled();
-
-  errorSpy.mockRestore();
-});
-
-test("renders three <a> elements", () => {
-  const { container } = render(<NavBar />);
-  expect(container.querySelectorAll("a")).toHaveLength(3);
-});
-
-test("displays the correct text for each <a> element", () => {
-  render(<NavBar />);
-  expect(screen.queryByText(/home/i)).toBeInTheDocument();
-  expect(screen.queryByText(/about/i)).toBeInTheDocument();
-  expect(screen.queryByText(/projects/i)).toBeInTheDocument();
-});
-
-test("each <a> element has the correct href attribute", () => {
-  render(<NavBar />);
-  expect(screen.queryByText(/home/i).href).toContain("#home");
-  expect(screen.queryByText(/about/i).href).toContain("#about");
-  expect(screen.queryByText(/projects/i).href).toContain("#projects");
-});
+export default NavBar;
